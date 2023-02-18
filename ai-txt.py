280bbd336cce257957e7149d88fba37e7516ed57
Mark-Zwaving <markzwaving@gmail.com>
2023-02-18 21:06:23 +0100

    Main function to new option module

--- ai-txt.py
+++ ai-txt.py
@@ -4,31 +4,26 @@ __author__     =  'Mark Zwaving'
 __email__      =  'markzwaving@gmail.com'
 __license__    =  'GNU General Public License (GPLv3)'
 __copyright__  =  'Copyright (C) Mark Zwaving. All rights reserved.'
-__version__    =  '0.0.4'
+__version__    =  '0.0.6'
 __status__     =  'Development'
 
 import sources.fn as fn
 import sources.txt as txt
+import sources.option as option
 
-try:
-    fn.console(txt.intro_api_txt, True)
-    while True:
-        fn.ask_model_temp()
+if __name__ == '__main__':
+    try:
+        fn.console(txt.intro_api_txt, True)
+        while True:
+            option.ai_txt(menu=False)
 
-        print(' ')
-        ok, question = fn.ask_multiline()
-        if not ok:
-            continue
+            if fn.quit():
+                break
 
-        ok = fn.process_question_txt(question)
+    except KeyboardInterrupt: # User interrupt quit the program with ctrl+c
+        pass
 
-        if fn.quit():
-            break
-
-except KeyboardInterrupt: # User interrupt quit the program with ctrl+c
-    pass
-
-fn.console(f'\n\n{txt.goodbye}', True)
+    fn.console(f'\n\n{txt.goodbye}', True)
 
 # {
 #   "choices": [
